ca062b7f2786e91f9aa029cfe0c2595cb0fbb0e3
zain <z3eiyn@gmail.com>
2023-04-04 23:18:33 +0700

    ubah data route web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,14 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-
+// Home
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/portfolio', [HomeController::class, 'portfolio']);
+Route::get('/services', [HomeController::class, 'services']);
+Route::get('/team', [HomeController::class, 'team']);
+Route::get('/testimonials', [HomeController::class, 'testimonials']);
 
 // Auth
 Route::get('/login', [AuthController::class, 'login'])->name('login');
